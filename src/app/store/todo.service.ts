866125a99f33f32137ddd0f15a112f73f087e121
profanis <prodromouf@gmail.com>
2020-10-22 10:49:00 +0300

    with service store

--- src/app/store/todo.service.ts
+++ src/app/store/todo.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Actions, ofActionDispatched, Store } from '@ngxs/store';
+import { Actions, Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 import { TodoModel } from './todo-state.model';
 import { AddTodo, UpdateTodo } from './todo.actions';
 import { TodoSelectors } from './todo.selectors';
@@ -9,23 +8,18 @@ import { TodoSelectors } from './todo.selectors';
 @Injectable()
 export class TodoService {
 
-  // @Select(TodoSelectors.items)
-  items$: Observable<TodoModel[]> = this.store.selectOnce(TodoSelectors.items)
+  items: TodoModel[]
 
+  @Select(TodoSelectors.items)
+  items$: Observable<TodoModel[]>
 
   constructor(private store: Store, actions$: Actions) {
-    actions$
-      .pipe(
-        ofActionDispatched(UpdateTodo, AddTodo),
-        debounceTime(500),
-      )
-      .subscribe(title => {
-        this.items$ = this.store.selectOnce(TodoSelectors.items)
-      });
+
   }
 
   add(title: string) {
     this.store.dispatch(new AddTodo(title))
+    this.items = this.store.selectSnapshot(TodoSelectors.items)
   }
 
   changeDescription(title: string, order: number) {
