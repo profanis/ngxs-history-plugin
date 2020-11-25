1be242f134475283e0439ab39f985f5ed2c1fd04
profanis <prodromouf@gmail.com>
2020-11-25 19:48:53 +0200

    added bootstrap styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngxs/store';
-import { TodoService } from './store/todo.service';
+import { Component } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { TodoModel } from './store/todo-state.model';
+import { AddTodo, UpdateTodo } from './store/todo.actions';
+import { TodoSelectors } from './store/todo.selectors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [TodoService]
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
+
+  @Select(TodoSelectors.items)
+  items$: Observable<TodoModel[]>
 
   newTitle: string
 
-  constructor(private store: Store, public todoService: TodoService) {}
+  constructor(private store: Store) {}
 
-  ngOnInit() {
+  add() {
+    this.store.dispatch(new AddTodo(this.newTitle))
+    this.newTitle = ''
   }
 
-  // add() {
-  //   this.store.dispatch(new AddTodo(this.newTitle))
-  //   this.newTitle = ''
-  // }
-
-  // changeDescription(title: string, order: number) {
-  //   this.store.dispatch(new UpdateTodo(order, title))
-  // }
+  changeDescription(title: string, order: number) {
+    this.store.dispatch(new UpdateTodo(order, title))
+  }
 }
